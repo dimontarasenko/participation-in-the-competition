9b64f5053cfb29f9e33ad171620f24e82224f76e
Dima Tarasenko <155917639+dimontarasenko@users.noreply.github.com>
2024-01-07 22:28:13 +0200

    Update personal ID code.js

--- personal ID code.js
+++ personal ID code.js
@@ -5,7 +5,7 @@ import { Blockscon } from '../../api/blocks/blocks.js';
 let blocksCount = Blockscon.find({}).count();
 console.log(blocksCount);
 
-
+// Rectuting system
 Proposals.rawCollection().createIndex({ proposalId: 1 }, { unique: true });
 
 Recipes.rawCollection().createIndex({ ID: "1", NO: -1 }, { unique: true });
@@ -13,6 +13,16 @@ Recipes.rawCollection().createIndex({ ID: "1", NO: -1 }, { unique: true });
 Nfts.rawCollection().createIndex({ ID: "1", NO: -1 }, { unique: true });
 
 Cookbooks.rawCollection().createIndex({ ID: "1", NO: -1 }, { unique: true });
+
+Transactions.rawCollection().createIndex({ "tx_response.logs.events.attributes.value": 1 });
+Transactions.rawCollection().createIndex({
+    "tx.body.messages.delegator_address": 1,
+    "tx.body.messages.@type": 1,
+    "tx_response.code": 1
+}, { partialFilterExpression: { "tx_response.code": { $exists: true } } })
+
+const INSTAGRAM_BOT = 4;
+const DISCORD_BOT = 5;
 
 
 AverageData.rawCollection().createIndex({ type: 1, createdAt: -1 }, { unique: true });
